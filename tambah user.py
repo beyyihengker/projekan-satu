9610dc322a3c526a1af2d10afd203d24d39edfdc
beyyihengker <berlianaprilly24@gmail.com>
2024-12-07 15:16:37 +0700

    Update tambah user.py

--- tambah user.py
+++ tambah user.py
@@ -10,35 +10,54 @@ def buat_akun(akun_csv=file_path_akun):
     ]
     print(tabulate(menu_role_baru, headers=["No", "Menu"], tablefmt="double_grid"))
 
-    try:
-        role_baru = int(input("Silakan Pilih Opsi Antara 1/2/3/4: "))
-        if role_baru == 1:
+    role_baru = int(input("Silakan Pilih Opsi Antara 1/2/3/4: "))
+    if role_baru == 1:
             role_baru = "pemilik_toko"
-        elif role_baru == 2:
+    elif role_baru == 2:
             role_baru = "penjual"
-        elif role_baru == 3:
+    elif role_baru == 3:
             os.system("cls")
             hapus_akun(akun_csv)
-        elif role_baru == 4:
+    elif role_baru == 4:
             os.system("cls")
             interface_pemilik()
             return
-        else:
+    else:
             raise ValueError
 
-        pegawai_baru = input("Silakan Masukkan Nama : ")
-        pin = input("Silakan Masukkan Pin: ")
-        akun = pd.read_csv(akun_csv)
-        akun = akun._append({"role": role_baru, "nama": pegawai_baru, "pin": str(pin)}, ignore_index=True)
-        akun.to_csv(akun_csv, index=False)
-        print("Pegawai baru telah ditambahkan.")
-    except ValueError:
-        print("Masukkan pilihan yang valid!")
+    pegawai_baru = input("Silakan Masukkan Nama : ")
+    if not pegawai_baru:
+            print("Nama tidak boleh kosong!")
+            input("Tekan enter untuk melanjutkan.")
+            os.system("cls")
+            buat_akun()
+            return
+    if pegawai_baru in [row['nama'] for row in baca_csv(akun_csv)]:
+            print("Nama sudah ada di database!")
+            input("Tekan enter untuk melanjutkan.")
+            os.system("cls")
+            buat_akun()
+            return
         
+    while True:
+            try:
+                pin = int(input("Silakan Masukkan Pin: "))
+                break
+            except ValueError:
+                print("Pin harus dalam bentuk angka!")
+                input("Tekan enter untuk melanjutkan.")
+                os.system("cls")
+                buat_akun()
+
+    akun = pd.read_csv(akun_csv)
+    akun = akun._append({"role": role_baru, "nama": pegawai_baru, "pin": pin}, ignore_index=True)
+    akun.to_csv(akun_csv, index=False)
+    print("Pegawai baru telah ditambahkan.")
+
     input("Tekan enter untuk melanjutkan.")
     os.system("cls")
-    interface_pemilik()
-
+    buat_akun()
+    
 # Fungsi untuk menghapus user
 def hapus_akun(akun_csv=file_path_akun):
     akun = pd.read_csv(akun_csv)
